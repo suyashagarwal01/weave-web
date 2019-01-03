1dd78ff1c0781ae2b2fcc86afc61b38513a4ef5b
Naman Maheshwari <namanmaheshwari97@gmail.com>
2019-01-03 17:03:52 +0530

    Added jQuery and Popper, ongoing work on 'Team' page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/team')
+def index():
+    return render_template('team.html')
+
 if __name__=='__main__':
     app.run(debug=True)
