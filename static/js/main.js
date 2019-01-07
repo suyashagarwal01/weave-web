9fff004671df02d4494608e9cdb3a67b91bbb67c
Naman Maheshwari <namanmaheshwari97@gmail.com>
2019-01-07 21:55:51 +0530

    Final-ish website

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,23 @@
 $(document).ready(function(){
   // Add smooth scrolling to all links
   var changehash="#x";
+  if($(window).scrollTop() > 200) {
+    $('#menu').addClass('scrolling');
+    $(".social_media li a").css({"color":"black"});
+  }
+
   $(".navbar a").on('click', function(event) {
 
     if (this.hash !== "") {
       event.preventDefault();
 
       var hash = this.hash;
- 
+
       $('html, body').stop().animate({
         scrollTop: $(hash).offset().top
-    }, 500, function() {
+      }, 500, function() {
         window.location.hash = hash;
-    });
+      });
     } // End if 
   });
 
@@ -22,23 +27,23 @@ $(document).ready(function(){
       event.preventDefault();
 
       var hash = this.hash;
- 
+
       $('html, body').stop().animate({
         scrollTop: $(hash).offset().top
-    }, 500, function() {
+      }, 500, function() {
         window.location.hash = hash;
-    });
+      });
     } // End if 
   });
 
   $(window).scroll(function() {
-  	if($(window).scrollTop() > 200) {
+    if($(window).scrollTop() > 200) {
       $('#menu').addClass('scrolling');
       $(".social_media li a").css({"color":"black"});
     }
     else {
-    $('#menu').removeClass('scrolling');
-    $(".social_media li a").css({"color":"white"});
+      $('#menu').removeClass('scrolling');
+      $(".social_media li a").css({"color":"white"});
     }
 
     // $('section').each(function () {
@@ -56,16 +61,16 @@ $(document).ready(function(){
   });
 });
 
-  //   $(document).on('scroll',function()
-  // {
-  //     $('.scrollx').each(function()
-  //     {
-  //       var data = this.hash;
-  //       console.log(data);
-  //         if ( $(this).offset().top < window.pageYOffset + 10 &&   $(this).offset().top + $(this).height() > window.pageYOffset + 10) 
-  //         {
-  //           window.location.hash = data;
-  //         }
-  // });
+//   $(document).on('scroll',function()
+// {
+//     $('.scrollx').each(function()
+//     {
+//       var data = this.hash;
+//       console.log(data);
+//         if ( $(this).offset().top < window.pageYOffset + 10 &&   $(this).offset().top + $(this).height() > window.pageYOffset + 10)
+//         {
+//           window.location.hash = data;
+//         }
+// });
 
-  // });
+// });
